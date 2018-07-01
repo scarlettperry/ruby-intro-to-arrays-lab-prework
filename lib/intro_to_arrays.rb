08f830fefb9e140754ea567949a1bddb13aa411d
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 01:57:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,6 +2,6 @@ def instantiate_new_array
   new_array = []
 end
 
-def array_with_two_elements
-  new_array = [element1, element2]
+def array_with_two_elements (array, element1, element2)
+  array = [element1, element2]
 end
